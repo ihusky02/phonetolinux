8d4495c6221bfb9b63d48abfbef414a5fcfe3eee
ihusky02 <stanislawtlolka@gmail.com>
2026-09-16 08:12:45 +0200

    Changing number version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "pl.stanislawtlolka.phonetolinux"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.4"
+        versionCode = 7
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
